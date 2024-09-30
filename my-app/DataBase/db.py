a1f960675b5399dd898dd50bd61ff1bc2427ee50
Newton Araújo | Developer <127565702+newton-araujo@users.noreply.github.com>
2024-09-29 21:11:23 -0300

    Update db.py

--- my-app/DataBase/db.py
+++ my-app/DataBase/db.py
@@ -19,25 +19,4 @@ class MeuBanco:
             print(f"Ocorreu um erro ao adicionar o documento: {e}")
             return False
 
-# # Criação da conexão com o MongoDB
-# username = "newtonaraujo6"
-# password = "Lorenzo@7045"  
-# encoded_password = quote_plus(password)
 
-# data_connection = f"mongodb+srv://{username}:{encoded_password}@systemmydoctor.8cxo4.mongodb.net/?retryWrites=true&w=majority&appName=SystemMyDoctor"
-# connection_db = MeuBanco(data_connection, 'SystemMyDoctor') 
-
-# # Exemplo de uso
-# dados = {
-#     "nome": "Newton de Paula Araujo",
-#     "cpf": "00000000000",
-#     "email": "teste@teste.com",
-#     "seguranca": {
-#         "password": "admin"
-#     }
-# }
-# colecao = connection_db.db['sua_colecao']
-# resultado = connection_db.adicionar_dados(colecao, dados)
-
-# if not resultado:
-#     print("Não foi possível adicionar os dados.")
